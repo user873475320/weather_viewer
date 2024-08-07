d5cef257075e0933b9d3a9a0fe161a37775a470d
Artem Pestrikov <user873475320@gmail.com>
2024-08-08 04:58:06 +0500

    refactor: change return type to Optional in 'getUserByLoginAndPassword' method

--- src/main/java/service/AuthenticationService.java
+++ src/main/java/service/AuthenticationService.java
@@ -13,11 +13,10 @@ public class AuthenticationService {
     private final UserDAO userDAO = new UserDAO();
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public User getUserByLoginAndPassword(String login, String password) {
+    public Optional<User> getUserByLoginAndPassword(String login, String password) {
         Optional<User> optionalUser = userDAO.findByLogin(login);
         return optionalUser
-                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
-                .orElse(null);
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()));
     }
 
     public void addUser(String login, String password) {
