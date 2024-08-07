11765c4b9f5d288fd18155572f5d9ed5cc8d8bf4
Artem Pestrikov <user873475320@gmail.com>
2024-08-08 04:56:55 +0500

    feat: add HttpSession utils class

---
+++ src/main/java/util/HttpSessionUtils.java
@@ -0,0 +1,30 @@
+package util;
+
+import entity.Session;
+import entity.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class HttpSessionUtils {
+
+    private HttpSessionUtils() {
+    }
+
+    public static void createAndSetUpHttpSession(HttpServletRequest req, Session session) {
+        HttpSession httpSession = req.getSession(true);
+        httpSession.setAttribute("authorizedUser", session.getUser());
+        httpSession.setAttribute("authorizedUserSession", session);
+    }
+
+    public static void createAndSetUpHttpSession(HttpServletRequest req, Session session, User user) {
+        HttpSession httpSession = req.getSession(true);
+        httpSession.setAttribute("authorizedUser", user);
+        httpSession.setAttribute("authorizedUserSession", session);
+    }
+
+    public static void clearHttpSessionData(HttpSession httpSession) {
+        httpSession.setAttribute("authorizedUserSession", null);
+        httpSession.setAttribute("authorizedUser", null);
+    }
+}
