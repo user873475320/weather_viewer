bc5177f3ddf9ed790eb189db39aaf6c04308ec5a
Artem Pestrikov <user873475320@gmail.com>
2024-08-08 05:00:07 +0500

    feat: add methods for getting sessions

--- src/main/java/service/SessionService.java
+++ src/main/java/service/SessionService.java
@@ -4,7 +4,11 @@ import dao.SessionDAO;
 import entity.Session;
 import entity.User;
 
+import javax.servlet.http.Cookie;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class SessionService {
@@ -26,7 +30,37 @@ public class SessionService {
         return sessionDAO.findById(sessionId).orElse(null);
     }
 
+    public Session getSessionByIdAndLoadUser(String sessionId) {
+        return sessionDAO.findByIdAndLoadUser(sessionId).orElse(null);
+    }
+
     public void deleteSession(String sessionId) {
         sessionDAO.deleteById(sessionId);
+    }
+
+    public Session getValidSessionAndDeleteIfNot(@NotNull Cookie[] cookies) {
+        List<Cookie> sessionCookies = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals("SESSIONID")).toList();
+        for (Cookie cookie : sessionCookies) {
+            String sessionId = cookie.getValue();
+            Session session = getSessionByIdAndLoadUser(sessionId);
+
+            if (session != null) {
+                if (LocalDateTime.now().isBefore(session.getExpiresAt())) {
+                    return session;
+                } else {
+                    deleteSession(sessionId);
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean getValidSessionAndDeleteIfNot(@NotNull Session session) {
+        if (LocalDateTime.now().isBefore(session.getExpiresAt())) {
+            return true;
+        } else {
+            deleteSession(session.getId());
+            return false;
+        }
     }
 }
