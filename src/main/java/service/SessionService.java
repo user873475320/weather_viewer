fe610535e47c0e3f8bf897a1e2690436620e467e
Artem Pestrikov <user873475320@gmail.com>
2024-08-10 01:26:20 +0500

    refactor: change some method names, return types to Optional and split some methods into several

--- src/main/java/service/SessionService.java
+++ src/main/java/service/SessionService.java
@@ -8,30 +8,43 @@ import javax.servlet.http.Cookie;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class SessionService {
 
     private final SessionDAO sessionDAO = new SessionDAO();
 
-    public Session createAndSaveSession(Long userId) {
-        Session session = new Session(
+    public Session getConfiguredSession(Long userId) {
+        return new Session(
                 UUID.randomUUID().toString(),
                 User.builder().id(userId).build(),
                 LocalDateTime.now().plusWeeks(1)
         );
+    }
 
+    public Optional<Session> findSessionWithLoadedUserByCookies(Cookie[] cookies) {
+        if (cookies != null) {
+            Optional<Cookie> sessionCookie = Arrays
+                    .stream(cookies)
+                    .filter(cookie -> cookie.getName().equals("SESSIONID"))
+                    .findAny();
+            return sessionCookie.flatMap(cookie -> findSessionWithLoadedUserById(cookie.getValue()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Session> findSessionWithLoadedUserById(String sessionId) {
+        return sessionDAO.findByIdAndLoadUser(sessionId);
+    }
+
+    public boolean checkIfSessionIsValid(@NotNull Session session) {
+        return LocalDateTime.now().isBefore(session.getExpiresAt());
+    }
+
+    public void saveSession(Session session) {
         sessionDAO.save(session);
-        return session;
-    }
-
-    public Session getSessionById(String sessionId) {
-        return sessionDAO.findById(sessionId).orElse(null);
-    }
-
-    public Session getSessionByIdAndLoadUser(String sessionId) {
-        return sessionDAO.findByIdAndLoadUser(sessionId).orElse(null);
     }
 
     public void deleteSession(String sessionId) {
