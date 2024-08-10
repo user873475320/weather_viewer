2bbbb098d0cd3cf0d6f9445938b2a0a562a2d5b8
Artem Pestrikov <user873475320@gmail.com>
2024-08-11 04:58:50 +0500

    feat: add new UserUtils class for extract logic(from servlet) of getting UserDTO from request

---
+++ src/main/java/util/UserUtils.java
@@ -0,0 +1,26 @@
+package util;
+
+import dto.UserLoginDTO;
+import dto.UserRegistrationDTO;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class UserUtils {
+
+    private UserUtils() {
+    }
+
+    public static UserLoginDTO getUserLoginDtoFromRequest(HttpServletRequest request) {
+        String login = request.getParameter("login").strip();
+        String password = request.getParameter("password").strip();
+        return new UserLoginDTO(login, password);
+    }
+
+    public static UserRegistrationDTO getUserRegistrationDtoFromRequest(HttpServletRequest request) {
+        String login = request.getParameter("login").strip();
+        String password = request.getParameter("password").strip();
+        String confirmPassword = request.getParameter("confirm-password").strip();
+
+        return new UserRegistrationDTO(login, password, confirmPassword);
+    }
+}
