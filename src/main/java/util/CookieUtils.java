929f7d9b3b70f01ac8a5bee6bdf1c054f86282c2
Artem Pestrikov <user873475320@gmail.com>
2024-08-11 04:54:29 +0500

    feat: add new method 'deleteSessionCookie'

--- src/main/java/util/CookieUtils.java
+++ src/main/java/util/CookieUtils.java
@@ -1,6 +1,7 @@
 package util;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtils {
 
@@ -8,7 +9,6 @@ public class CookieUtils {
     }
 
     public static Cookie createConfiguredCookie(String sessionId) {
-        // TODO: Maybe configure this cookie in another way
         Cookie cookie = new Cookie("SESSIONID", sessionId);
         cookie.setMaxAge(60*60*24*7);
         cookie.setSecure(true);
@@ -16,5 +16,12 @@ public class CookieUtils {
         cookie.setPath("/");
 
         return cookie;
+    }
+
+    public static void deleteSessionCookie(HttpServletResponse response) {
+        Cookie cookie = createConfiguredCookie("");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
     }
 }
