30c12b15aa8840e96ef006f8ac1f086fa36cfe3a
Artem Pestrikov <user873475320@gmail.com>
2024-08-24 07:10:21 +0500

    feat: add LocationService

---
+++ src/main/java/service/LocationService.java
@@ -0,0 +1,38 @@
+package service;
+
+import dao.LocationDAO;
+import dto.LocationDTO;
+import entity.Location;
+import entity.User;
+
+import java.util.List;
+
+public class LocationService {
+
+    private final LocationDAO locationDAO = new LocationDAO();
+
+    public List<LocationDTO> findUserLocationDTOs(Long userId) {
+        return locationDAO.findLocationsByUserId(userId)
+                .stream()
+                .map(location -> new LocationDTO(location.getName(), location.getLatitude(), location.getLongitude()))
+                .toList();
+    }
+
+    public void saveLocation(LocationDTO locationDTO, Long userId) {
+        locationDAO.save(Location.builder()
+                    .name(locationDTO.getName())
+                    .latitude(locationDTO.getLatitude())
+                    .longitude(locationDTO.getLongitude())
+                    .user(User.builder().id(userId).build())
+                .build());
+    }
+
+    public void deleteLocation(LocationDTO locationDTO, Long userId) {
+        locationDAO.delete(Location.builder()
+                    .name(locationDTO.getName())
+                    .latitude(locationDTO.getLatitude())
+                    .longitude(locationDTO.getLongitude())
+                    .user(User.builder().id(userId).build())
+                .build());
+    }
+}
