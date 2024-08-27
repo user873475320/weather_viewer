385b191075be6215bb14c765ae840e5f1f2d678c
Artem Pestrikov <user873475320@gmail.com>
2024-08-28 03:35:39 +0500

    feat: add checking of correctness of user parameters

--- src/main/java/util/UserUtils.java
+++ src/main/java/util/UserUtils.java
@@ -2,23 +2,34 @@ package util;
 
 import dto.UserLoginDTO;
 import dto.UserRegistrationDTO;
+import exception.client.InvalidUserRequestException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class UserUtils {
 
     private UserUtils() {}
 
     public static UserLoginDTO getUserLoginDtoFromRequest(HttpServletRequest request) {
-        String login = request.getParameter("login").strip();
-        String password = request.getParameter("password").strip();
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if (login == null || password == null) {
+            throw new InvalidUserRequestException("Some of the parameters are missing", HttpServletResponse.SC_BAD_REQUEST);
+        }
+
         return new UserLoginDTO(login, password);
     }
 
     public static UserRegistrationDTO getUserRegistrationDtoFromRequest(HttpServletRequest request) {
-        String login = request.getParameter("login").strip();
-        String password = request.getParameter("password").strip();
-        String confirmPassword = request.getParameter("confirm-password").strip();
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirm-password");
+
+        if (login == null || password == null || confirmPassword == null) {
+            throw new InvalidUserRequestException("Some of the parameters are missing", HttpServletResponse.SC_BAD_REQUEST);
+        }
 
         return new UserRegistrationDTO(login, password, confirmPassword);
     }
