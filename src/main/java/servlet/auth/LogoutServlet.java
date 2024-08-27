f329f79886778f2b8c40479ba4dcd7f82fcaa7f9
Artem Pestrikov <user873475320@gmail.com>
2024-08-28 03:51:32 +0500

    refactor: add extending of BaseServlet and change the way of getting session from http session

--- src/main/java/servlet/auth/LogoutServlet.java
+++ src/main/java/servlet/auth/LogoutServlet.java
@@ -1,36 +1,31 @@
 package servlet.auth;
 
-import entity.Session;
-import exception.ExceptionHandler;
 import service.SessionService;
+import servlet.BaseServlet;
 import util.CookieUtils;
+import util.HttpSessionUtils;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet("/auth/logout")
-public class LogoutServlet extends HttpServlet {
+public class LogoutServlet extends BaseServlet {
     private final SessionService sessionService = new SessionService();
-    private final ExceptionHandler exceptionHandler = new ExceptionHandler();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            HttpSession httpSession = req.getSession();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession httpSession = req.getSession();
 
-            // Remove session from DB
-            sessionService.delete(((Session) httpSession.getAttribute("authorizedUserSession")).getId());
-            // Remove session from HttpSession
-            httpSession.invalidate();
-            // Remove cookie with SESSIONID
-            CookieUtils.deleteSessionCookie(resp);
+        // Remove session from DB
+        sessionService.delete(HttpSessionUtils.getSessionFromHttpSession(httpSession).getId());
+        // Remove session from HttpSession
+        httpSession.invalidate();
+        // Remove cookie with SESSIONID
+        CookieUtils.deleteSessionCookie(resp);
 
-            resp.sendRedirect("/index");
-        } catch (Exception e) {
-            exceptionHandler.handle(e);
-        }
+        resp.sendRedirect("/index");
     }
 }
