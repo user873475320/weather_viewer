aa21557bd0773a744584416e94fde0a9c52f3326
Artem Pestrikov <user873475320@gmail.com>
2024-08-28 03:43:35 +0500

    feat: add LocationServlet

---
+++ src/main/java/servlet/LocationServlet.java
@@ -0,0 +1,71 @@
+package servlet;
+
+import dto.LocationDTO;
+import entity.Session;
+import exception.client.InvalidUserRequestException;
+import service.LocationService;
+import service.OpenWeatherApiService;
+import util.HttpSessionUtils;
+import util.LocationUtils;
+import validation.validators.LocationExistenceValidator;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/location")
+public class LocationServlet extends BaseServlet {
+
+    private final LocationExistenceValidator locationExistenceValidator = new LocationExistenceValidator();
+    private final LocationService locationService = new LocationService();
+    private final OpenWeatherApiService openWeatherApiService = new OpenWeatherApiService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Session session = HttpSessionUtils.getSessionFromHttpSession(req.getSession());
+
+        LocationDTO locationDTO = LocationUtils.getUserLoginDtoOnlyWithNameFromRequest(req);
+        Set<ConstraintViolation<LocationDTO>> violations = validator.validate(locationDTO);
+
+        if (!violations.isEmpty()) {
+            throw new InvalidUserRequestException("Data is incorrect(name too long)", HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        context.setVariable("login", session.getUser().getLogin());
+        context.setVariable("weatherDtoList", openWeatherApiService.getWeatherData(locationDTO.getName()));
+        templateEngine.process("search_results", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        Session session = HttpSessionUtils.getSessionFromHttpSession(req.getSession());
+
+        LocationDTO locationDTO = LocationUtils.getUserLoginDtoFromRequest(req);
+        Set<ConstraintViolation<LocationDTO>> violations = validator.validate(locationDTO);
+        boolean isExist = locationExistenceValidator.isExist(locationDTO, session.getUser().getId());
+
+        if (!violations.isEmpty() || isExist) {
+            throw new InvalidUserRequestException("Location already exists or location data is incorrect", HttpServletResponse.SC_BAD_REQUEST, true);
+        }
+
+        locationService.saveLocation(locationDTO, session.getUser().getId());
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        Session session = HttpSessionUtils.getSessionFromHttpSession(req.getSession());
+
+        LocationDTO locationDTO = LocationUtils.getUserLoginDtoFromRequest(req);
+        Set<ConstraintViolation<LocationDTO>> violations = validator.validate(locationDTO);
+        boolean isExist = locationExistenceValidator.isExist(locationDTO, session.getUser().getId());
+
+        if (!violations.isEmpty() || !isExist) {
+            throw new InvalidUserRequestException("Location doesn't exist or location data is incorrect", HttpServletResponse.SC_BAD_REQUEST, true);
+        }
+
+        locationService.deleteLocation(locationDTO, session.getUser().getId());
+    }
+}
